f1598659ca7c90e86fd038d5a68d55e2da929af0
Sarah Johnston <sarahjohnston502@gmail.com>
2016-02-18 14:41:49 +0000

    Multiple recipe pages displaying ingredients and methods.

--- index.php
+++ index.php
@@ -1,34 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>test</title>
-    </head>
-    <body>
-        <?php
+    <?php
         $servername = "localhost";
         $username = "root";
         $password = "";
         $database = "recipes";
         $port = 3306;
-        
         // create connection
         $conn = mysqli_connect($servername, $username, $password, $database, $port);
-                
         // check connection
         if(!$conn) {
             die("Conection failed: " . mysqli_connect_error());
         }
-        
         $sql = "SELECT recipe_id, recipe_name FROM recipes";
         $result = mysqli_query($conn, $sql);
         if (!$result) {
            die(mysqli_error($conn));
         }
+        
+        ?>
+    <head>
+        <meta charset="UTF-8">
+        <title>Recipes</title>
+    </head>
+    <body>
+        <h1>Recipes</h1>
+        <?php
         if (mysqli_num_rows($result) > 0){
             while($row = mysqli_fetch_assoc($result)){
-                echo "<a href = 'recipe.php?id=" . $row["recipe_id"] . "'>" 
-                        . $row["recipe_name"] . "</a><br>";
+                $recipe_id = $row['recipe_id'];
+                $recipe_name = $row['recipe_name'];
+        ?>
+        <form action="recipe.php" method="POST">
+            <input type="hidden" name="id" value="<?=$recipe_id?>" />
+            <input type="submit" value="<?=$recipe_name?>"/>
+        </form><br />
+        <?php 
             } 
         }
         else {
--- recipe.php
+++ recipe.php
@@ -1,11 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
+    <?php
             $servername = "localhost";
             $username = "root";
             $password = "";
@@ -13,16 +8,52 @@
             $port = 3306;
             // create connection
             $conn = mysqli_connect($servername, $username, $password, $database, $port);
-
-            $id = mysqli_real_escape_string($conn, $_GET['id']);
-            $sql = "SELECT `recipe_name` FROM `recipes` WHERE `recipe_id` = " 
-                    .$id. " LIMIT 1";
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
-
-            // Page content
-            echo $row['recipe_name'];
+            $id = mysqli_real_escape_string($conn, $_POST['id']);
             
+            $recipe_name_sql = "SELECT recipe_name, recipe_method FROM recipes WHERE recipe_id = " 
+                    .$id. " LIMIT 1";
+            $ingredients_sql = "SELECT ri.recipe_ingredient_amount, "
+                    . "u.unit_name, i.recipe_ingredient FROM ingredients i "
+                    . "INNER JOIN recipe_ingredients ri "
+                    . "ON i.recipe_ingredient_id=ri.recipe_ingredient_id "
+                    . "INNER JOIN units u ON ri.unit_id=u.unit_id "
+                    . "WHERE recipe_id = " .$id;
+            $recipe_details = mysqli_fetch_assoc(mysqli_query($conn, $recipe_name_sql));
+            $recipe_name = $recipe_details['recipe_name'];
+            $recipe_method = $recipe_details['recipe_method'];
+            $ingredients = mysqli_query($conn, $ingredients_sql);
+            if (!$ingredients) {
+                die(mysqli_error($conn));
+            }
         ?>
+    <head>
+        <meta charset="UTF-8">
+        <title><?=$recipe_name;?></title>
+    </head>
+    <body>
+        <article>
+            <h1><?=$recipe_name;?></h1>
+            <table>
+                <h3>Ingredients</h3>
+                <?php 
+                if (mysqli_num_rows($ingredients) > 0){
+                 while($row = mysqli_fetch_assoc($ingredients)){
+                     $ingredient_amount = $row['recipe_ingredient_amount'];
+                     $ingredient_unit = $row['unit_name'];
+                     $ingredient_name = $row['recipe_ingredient'];
+                     ?>
+                <tr>
+                    <td><?=$ingredient_amount?></td>
+                    <td><?=$ingredient_unit?></td>
+                    <td><?=$ingredient_name?></td>
+                </tr>
+                 <?php
+                     }
+                 }
+                ?>
+            </table>
+            <h3>Method</h3>
+            <p><?=$recipe_method;?></p>
+        </article>
     </body>
 </html>
